bee7fadc71dd21f29082a65e14892948fdd015ff
Andrey Baryshev (Barkos) <andrey.bar66@gmail.com>
2023-12-07 15:27:32 +0500

    Update D7M12Y23.15
    convertNumber:
    Убрал лишние комментарии
    Добавил информацию об ошибках

--- HomeTask (7.12.2023)/convertNumber.cpp
+++ HomeTask (7.12.2023)/convertNumber.cpp
@@ -8,10 +8,6 @@ using std::string;
 
 // Алфавит
 string alphabet = "0123456789ABCDEF";
-
-// Convert - перевод числа number в base-систему
-// number - число, которое переводим
-// base - основание системы счисления
 string Convert(int number, int base, string result = "") {
     if (number < base) return alphabet[number] + result;
     return Convert(number / base, base, alphabet[number % base] + result);
@@ -27,8 +23,8 @@ int main() {
     int number, base;
     cout << "Enter number (number >= 0): "; cin >> number;
     cout << "Enter base (2 <= base <= 16): "; cin >> base;
-    if (number < 0) { cout << "" << endl; return 0; };
-    if (base < 2 || 16 < base) { cout << "" << endl; return 0; };
+    if (number < 0) { cout << "The number must be positive" << endl; return 0; };
+    if (base < 2 || 16 < base) { cout << "The base should be from 2 to 16" << endl; return 0; };
     cout << number << " (10) = " << Convert(number, base) << " (" << base << ")" << endl;
     return 0;
 }
