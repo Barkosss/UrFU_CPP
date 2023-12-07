67c72de8bc56df0a11b39ec924370e088cfd41a3
Andrey Baryshev (Barkos) <andrey.bar66@gmail.com>
2023-12-07 15:22:17 +0500

    Update D7M12Y23.15
    convertNumber:
    Исправлена ошибка перевода

--- HomeTask (7.12.2023)/convertNumber.cpp
+++ HomeTask (7.12.2023)/convertNumber.cpp
@@ -1,25 +1,20 @@
 #include<iostream>
+#include<string>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
 
-int i = 0;
-char ArrayNumber[1] { '0' };
-char ArrayBase[6] { 'A', 'B', 'C', 'D', 'E', 'F' };
+// Алфавит
+string alphabet = "0123456789ABCDEF";
 
 // Convert - перевод числа number в base-систему
 // number - число, которое переводим
 // base - основание системы счисления
-int remain; // Остаток числа
-int Convert(int number, int base) {
-    if (number == 0) return 1;
-    remain = number % base;
-    cout << endl << remain;
-    if (remain > 9) ArrayNumber[i] = ArrayBase[remain - 10];
-    else ArrayNumber[i] = remain;
-    i++;
-    return Convert(number / base, base);
+string Convert(int number, int base, string result = "") {
+    if (number < base) return alphabet[number] + result;
+    return Convert(number / base, base, alphabet[number % base] + result);
 }
 
 
@@ -34,9 +29,6 @@ int main() {
     cout << "Enter base (2 <= base <= 16): "; cin >> base;
     if (number < 0) { cout << "" << endl; return 0; };
     if (base < 2 || 16 < base) { cout << "" << endl; return 0; };
-    cout << number << " (10) = ";
-    Convert(number, base);
-    for(int index = i - 1; index >= 0; index--) cout << ArrayNumber[index];
-    cout << " (" << base << ")" << endl;
+    cout << number << " (10) = " << Convert(number, base) << " (" << base << ")" << endl;
     return 0;
 }
