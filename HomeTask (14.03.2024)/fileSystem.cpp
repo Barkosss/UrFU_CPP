8737edfbeed6fe45985f5e4129b817aa090e04df
Andrey Baryshev (Barkos) <andrey.bar66@gmail.com>
2024-03-14 10:15:46 +0500

    Update Output Format
    Изменён формат вывода информации

--- HomeTask (14.03.2024)/fileSystem.cpp
+++ HomeTask (14.03.2024)/fileSystem.cpp
@@ -51,10 +51,10 @@ void fileSystem(std::ifstream&readFile, std::ofstream&writeFile) {
 
 
     // Вывод информации
-    writeFile << "Train\t|\tStation\t| Timestamp                       |" << std::endl;
-    writeFile << "--------|-----------|---------------------------------|" << std::endl;
+    writeFile << "#Train\t|\tStation\t| Timestamp\t\t\t(Date)\t\t\t\t(Seconds)   |" << std::endl;
+    writeFile << "--------|-----------|---------------------------------------------------|" << std::endl;
     for(int indexTrain = 0; indexTrain < counterTrains; indexTrain++) {
-        writeFile << "#" << trains[indexTrain].numberTrain << "\t|\t" << trains[indexTrain].destination << "\t| Timestamp:\t" << ConvertTimestamp(trains[indexTrain].timestamp) << std::endl;
+        writeFile << "#" << trains[indexTrain].numberTrain << "\t|\t" << trains[indexTrain].destination << "\t| Timestamp:\t" << ConvertTimestamp(trains[indexTrain].timestamp) << "\t|\t" << trains[indexTrain].timestamp << "\t|" << std::endl;
     }
 
     std::cout << "Write end" << std::endl;
